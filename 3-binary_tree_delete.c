017c3725c5e7c14646949669bfecef52598f2d92
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-04-14 16:30:46 +0100

    task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Function that
+ * deletes an entire binary tree.
+ *
+ * @tree: binary tree
+ *
+ * Returns: Nothing
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
