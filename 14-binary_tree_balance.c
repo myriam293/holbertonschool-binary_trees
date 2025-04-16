a149ba08876dc2a59950ee9c14a0cad58fb796a7
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-04-16 11:50:34 +0100

    task 14 upgrade

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -23,6 +23,14 @@ int binary_tree_balance(const binary_tree_t *tree)
 	return (left_height - right_height);
 }
 
+/**
+ * binary_tree_height - gets the height of a binary tree
+ *
+ * @tree: root node to draw height from for tree
+ *
+ * Return: size_t representing height, 0 on failure or NULL.
+ */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_size = 0;
