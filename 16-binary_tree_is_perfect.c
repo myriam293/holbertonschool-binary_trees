64b8d5e0f62b6d290d6cf8a117d0fc82d2322d5f
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-04-18 11:52:16 +0100

    perfect is not perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -29,12 +29,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	if (!tree->left && tree->right)
+	if ((!tree->left && tree->right) || (tree->left && !tree->right))
+		return (0);
+	if (!tree->left && !tree->right)
 		return (1);
-	if (tree->left && tree->right)
-		return (binary_tree_is_full(tree->left) &&
-				binary_tree_is_full(tree->right));
-	return (0);
+	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 }
 
 /**
