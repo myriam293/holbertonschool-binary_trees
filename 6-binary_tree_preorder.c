9ebd2409da59b69aa2dae30d515c1cab6382fe9f
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-04-18 16:29:40 +0100

    task 6 betty

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -3,13 +3,11 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_preorder - A function that goes through
-a binary tree using pre-order traversal.
-
-* @tree: const pointer to root node to begin traversal at.
-
-* @func: function to call on tree node's data.
-*/
+ * binary_tree_preorder - A function that goes through
+ * a binary tree using pre-order traversal.
+ * @tree: const pointer to root node to begin traversal at.
+ * @func: function to call on tree node's data.
+ */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
